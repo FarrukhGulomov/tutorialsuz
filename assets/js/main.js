a0ab325a276e3fdc3c1c2c57e0bac417d20a9a92
Farrukh Gulomov <@frankywht@gmail.com>
2021-02-02 23:30:31 +0500

    --- Auto-Git Commit ---

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,15 +14,26 @@ ready(function() {
   const menuTop = document.querySelector(".menu-top");
   const OpenBtn = document.getElementById("menu-toggle");
   const menuText =document.querySelector(".menu-text");
+  const closeBtn =document.getElementById("close-btn");
+  const navbarMenu =document.querySelector(".navbar-menu");
   OpenBtn.addEventListener('click',function(){
    
     
     menuTop.style.zIndex="3";
     menuTop.style.width="270px"; 
-     
-    
+    OpenBtn.style.display="none";
+    closeBtn.style.zIndex="10";
     
   });
+  closeBtn.addEventListener('click',function(event){
+
+  event.preventDefault();
+
+    navbarMenu.style.opacity="0";
+    menuTop.style.opacity="0";
+
+  });
+
  
 
 
