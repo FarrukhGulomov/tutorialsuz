43b6b6db46754741887ed0fa032f3ced5baed120
Farrukh Gulomov <@frankywht@gmail.com>
2021-02-05 22:11:51 +0500

    --- Auto-Git Commit ---

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,64 @@
 const ready = function (cb) {
-  // Check if the `document` is loaded completely
-  document.readyState === "loading"
-    ? document.addEventListener("DOMContentLoaded", function (e) {
-        cb();
-      })
-    : cb();
+    // Check if the `document` is loaded completely
+    document.readyState === "loading"
+        ? document.addEventListener("DOMContentLoaded", function (e) {
+            cb();
+        })
+        : cb();
 };
 
 // Usage
-ready(function() {
-  
+ready(function () {
+
 // MENU BTN TO OPEN RIGHT DOOR  
-  const menuTop = document.querySelector(".menu-top");
-  const OpenBtn = document.getElementById("menu-toggle");
-  const menuText =document.querySelector(".menu-text");
-  const closeBtn =document.getElementById("close-btn");
-  const navbarMenu =document.querySelector(".navbar-menu");
-  const mainBg = document.querySelector(".main-bg");
-  var is =false;
-  OpenBtn.addEventListener('click',function(){
-   
-    menuTop.style.zIndex="9";
-    menuTop.style.width="270px";
-    menuTop.style.right="0";
-    //OpenBtn.style.display="none";
-    closeBtn.style.zIndex="10";
-   OpenBtn.style.right="200px";
-   // menuTop.style.visibility="visible";
-    menuTop.style.display="block";
-  // menuText.style.display="none";
-
-OpenBtn.addEventListener('mouseover',function(){
-  OpenBtn.style.borderColor="transparent";
- 
-  
-
-
-  });
-
-  });
-  
-  closeBtn.addEventListener('click',function(event){
-
-  event.preventDefault();
-    
-    //navbarMenu.style.opacity="0";
-    //menuTop.style.opacity="0";
-    closeBtn.style.zIndex="-100";
-    OpenBtn.style.display="block";
-    menuTop.style.width="0px";
-    OpenBtn.style.right="5%";
-   // navbarMenu.style.display="none";
-    is=true;
-  });
-
- 
-
-
- 
+    const menuTop = document.querySelector(".menu-top");
+    const OpenBtn = document.getElementById("menu-toggle");
+    const menuText = document.querySelector(".menu-text");
+    const closeBtn = document.getElementById("close-btn");
+    const navbarMenu = document.querySelector(".navbar-menu");
+    const mainBg = document.querySelector(".main-bg");
+    var is = false;
 
 
 
 
+    OpenBtn.addEventListener('click', function () {
 
-   
+        //  menuTop.classList.toggle("d-block");
+
+
+        menuTop.style.display = "block";
+        menuTop.style.zIndex = "9";
+        menuTop.style.width = "270px";
+        menuTop.style.right = "0";
+        //OpenBtn.style.display="none";
+        closeBtn.style.zIndex = "10";
+        OpenBtn.style.right = "200px";
+        // menuTop.style.visibility="visible";
+
+        // menuText.style.display="none";
+
+        OpenBtn.addEventListener('mouseover', function () {
+            OpenBtn.style.borderColor = "transparent";
+
+
+        });
+
+    });
+
+    closeBtn.addEventListener('click', function (event) {
+
+        event.preventDefault();
+
+        //navbarMenu.style.opacity="0";
+        //menuTop.style.opacity="0";
+        closeBtn.style.zIndex = "-100";
+        OpenBtn.style.display = "block";
+        menuTop.style.width = "0px";
+        OpenBtn.style.right = "5%";
+         menuTop.style.display="none";
+        // is=true;
+    });
+
+
 });
